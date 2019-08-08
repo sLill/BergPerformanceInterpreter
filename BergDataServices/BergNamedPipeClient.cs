45ebb849875d862cab0673ea590120d78b01339e
Sam <samuelturnerlill@gmail.com>
2019-08-07 23:39:37 -0500

    -

---
+++ BergDataServices/BergNamedPipeClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BergDataServices
+{
+    public class BergNamedPipeClient
+    {
+        public void BergNamedPipeClient()
+        {
+            using (NamedPipeClientStream pipeClient =
+            new NamedPipeClientStream(".", "testpipe", PipeDirection.In))
+            {
+
+                // Connect to the pipe or wait until the pipe is available.
+                Console.Write("Attempting to connect to pipe...");
+                pipeClient.Connect();
+
+                Console.WriteLine("Connected to pipe.");
+                Console.WriteLine("There are currently {0} pipe server instances open.",
+                   pipeClient.NumberOfServerInstances);
+                using (StreamReader sr = new StreamReader(pipeClient))
+                {
+                    // Display the read text to the console
+                    string temp;
+                    while ((temp = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine("Received from server: {0}", temp);
+                    }
+                }
+            }
+        }
+    }
+}
